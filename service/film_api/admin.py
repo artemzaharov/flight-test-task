3d594a4219f5de371978ee3cc3172d8f31880447
Artem Zakharov <artem.zakharov.development@gmail.com>
2023-03-07 03:24:43 +0300

    найтройка админки, в том числе фильтрации для полей manyToMany

--- service/film_api/admin.py
+++ service/film_api/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from film_api.models import *
+from film_api.tools import *
 
-# Register your models here.
+
+class ContentAdmin(admin.ModelAdmin):
+    exclude = ('parents_count',)
+    filter_horizontal = ('parentChannels',)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "parentChannels":
+            # возвращаем только те каналы, которые не содержат подканалов
+            kwargs["queryset"] = Channel.objects.filter(subchannels_count=0)
+        return super(ContentAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
+
+class ChannelAdmin(admin.ModelAdmin):
+    exclude = ('parents_count', 'content_count', 'subchannels_count')
+    filter_horizontal = ('parentChannels',)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "parentChannels":
+            # получаем все каналы, которые могут быть родительскими, т.е. которые не содержат контент
+            channelsWithoutContent = Channel.objects.filter(content_count=0) 
+            if 'object_id' in request.resolver_match.kwargs:
+                # получаем  канал, для которого запрашиваем список
+                pk = request.resolver_match.kwargs['object_id']
+                channel = Channel.objects.get(pk=pk)
+                # получаем список всех потомков (тут рекурсия вплоть до листьев)
+                all_descendants = recursive_query_all_descendans_channels(channel)
+                # выбираем id потомков
+                all_descendants = [el.id for el in all_descendants]
+                channelsWithoutContent =  channelsWithoutContent.exclude(pk__in=all_descendants)
+            # возрващаем все потенциальные родительские каналы, не являющиеся потомком текущего
+            kwargs["queryset"] = channelsWithoutContent
+        return super(ChannelAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
+    
+admin.site.register(Content, ContentAdmin)
+admin.site.register(Channel, ChannelAdmin)
--- service/film_api/tools.py
+++ service/film_api/tools.py
@@ -3,10 +3,18 @@ from film_api.support_types import *
 from film_api.models import *
 
 def channel_to_content_childs(channel):
-    return [rel.contentFK for rel in channel.channel_contents.get_queryset()]
+    return channel.child_contents.all()
 
 def channel_to_subchannels(channel):
-    return  [rel.channelFK for rel in channel.channel_subchannels.get_queryset()]
+    return  channel.child_channels.all()
 
 def get_channel_forest():
     return ChannelForest(Channel.objects.filter(parents_count=0))
+
+def recursive_query_all_descendans_channels(channel):
+    if channel.child_contents.count() > 0:
+        return [channel]
+    ret = [channel]
+    for subchannel in channel.child_channels.all():
+        ret.extend(recursive_query_all_descendans_channels(subchannel))
+    return ret
